ccaa9eed60976a3f842ed9bbcff47d03b6b963ac
strawhat404 <yosephtesfaye27@gmail.com>
2025-02-14 08:36:38 +0300

    Views function

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    lottery_number = models.CharField(max_length=8, unique=True)
--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.db.models import Min
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            last_number = User.objects.aggregate(Min('lottery_number'))['lottery_number__min']
+            next_number = str(int(last_number) - 1) if last_number else '88888888'
+            serializer.save(lottery_number=next_number)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
